93f6603a7632146c443f0b1a2e9058e66feebd3e
Lucaslfs <lucas.limalfss@gmail.com>
2025-07-21 11:27:34 -0300

    Cap 8 Finalizado

--- interval.h
+++ interval.h
@@ -1,11 +1,13 @@
 #ifndef INTERVAL_H
 #define INTERVAL_H
+#include <limits>
+
 
 class interval {
   public:
     double min, max;
 
-    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+    interval() : min(+std::numeric_limits<double>::infinity()), max(-std::numeric_limits<double>::infinity()) {} // Default interval is empty
 
     interval(double min, double max) : min(min), max(max) {}
 
@@ -21,10 +23,16 @@ class interval {
         return min < x && x < max;
     }
 
+      double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
+
     static const interval empty, universe;
 };
 
-const interval interval::empty    = interval(+infinity, -infinity);
-const interval interval::universe = interval(-infinity, +infinity);
+const interval interval::empty    = interval(+std::numeric_limits<double>::infinity(), -std::numeric_limits<double>::infinity());
+const interval interval::universe = interval(-std::numeric_limits<double>::infinity(), +std::numeric_limits<double>::infinity());
 
 #endif
